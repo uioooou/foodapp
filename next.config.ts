171c00bdd83025387963486ab8b377aa238d71ee
jasperliew <yikpui@wtech.software>
2025-05-31 02:28:40 +0800

    fix5\

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
-// const isProduction = process.env.NODE_ENV === "production";
+const isGithubPages = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  
+  basePath: isGithubPages ? "/foodapp" : "",
+  assetPrefix: isGithubPages ? "/foodapp/" : "",
   distDir: "dist",
   images: {
     unoptimized: true, // Disable image optimization for static export
