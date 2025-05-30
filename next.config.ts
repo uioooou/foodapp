cb65d54406a0ce2d9a2650ef5001580b8b7b64d7
jasperliew <yikpui@wtech.software>
2025-05-31 00:27:32 +0800

    build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
